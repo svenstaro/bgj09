cda973c077934a5650bf0b1f6883481717b56f92
Sven-Hendrik Haase <sh@lutzhaase.com>
2015-06-13 17:18:17 +0200

    Actually push states

---
+++ src/main_state.hpp
@@ -0,0 +1,19 @@
+#ifndef STATE_MAIN_HPP
+#define STATE_MAIN_HPP
+
+#include "game.hpp"
+
+#include "strapon/state/state.hpp"
+
+class MainState : public State
+{
+public:
+    MainState(Game *game)
+    int init();
+    void update(float dt);
+
+private:
+    Game *m_game;
+};
+
+#endif /* end of include guard: STATE_MAIN_HPP */
