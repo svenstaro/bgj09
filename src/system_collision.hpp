0516debb1c29535a89a078f4320751205745da67
Sven-Hendrik Haase <sh@lutzhaase.com>
2015-06-14 14:59:11 +0200

    Misisng file

---
+++ src/system_collision.hpp
@@ -0,0 +1,29 @@
+#ifndef COLLISION_SYSTEM_CPP
+#define COLLISION_SYSTEM_CPP
+
+#include "events.hpp"
+#include "component_position.hpp"
+
+#include "entityx/entityx.h"
+
+#include <glm/vec2.hpp>
+#include <SDL2/SDL.h>
+#include <iostream>
+
+class CollisionSystem : public entityx::System<CollisionSystem> {
+public:
+	void update(entityx::EntityManager &es, entityx::EventManager &events, double dt) {	
+		entityx::ComponentHandle<Position> first_position, second_position;
+		for(entityx::Entity first_entity : es.entities_with_components(first_position)) {
+            for(entityx::Entity second_entity : es.entities_with_components(second_position)) {
+                if (first_entity != second_entity &&
+                        SDL_HasIntersection(&first_position->get_rect(), &second_position->get_rect())) {
+                    events.emit<CollisionEvent>(first_entity, second_entity);
+                    std::cout << "lol" << std::endl;
+                }
+            }
+        }
+    }
+};
+
+#endif
