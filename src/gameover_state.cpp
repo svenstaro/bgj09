a255da78f489eef34c2d80de972664e61663f5b7
NodmGatall <oliverheidmann@hotmail.de>
2015-06-14 22:49:09 +0200

    Fix game over text

--- src/gameover_state.cpp
+++ src/gameover_state.cpp
@@ -28,4 +28,14 @@ void GameoverState::update(double dt) {
             }
         }
     }
+
+    SDL_SetRenderDrawColor(m_game->get_renderer(), 0, 100, 200, 255);
+    SDL_RenderClear(m_game->get_renderer());
+    SDL_Color color{200, 100, 0, 200};
+    SDL_Surface *font_surf = TTF_RenderText_Blended(m_game->get_res_manager().get_font("font20"), "Game Over :(", color);
+    SDL_Texture *font_tex = SDL_CreateTextureFromSurface(m_game->get_renderer(), font_surf);
+    SDL_RenderCopy(m_game->get_renderer(), font_tex, nullptr, nullptr);
+    SDL_FreeSurface(font_surf);
+    SDL_DestroyTexture(font_tex);
+    SDL_RenderPresent(m_game->get_renderer());
 }
